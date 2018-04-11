8ca58a68b5cd563c6c31872b9d644c699643ca4c
dixitomkar1809 <omedxt@gmail.com>
2018-04-11 11:54:08 -0500

    spark stream working there is some error in the sentimental analysis

--- spark.py
+++ spark.py
@@ -1,5 +1,15 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.streaming import StreamingContext
+from pycorenlp import StanfordCoreNLP
+
+ScNLP = StanfordCoreNLP('http://localhost:3000')
+
+def sentimentalAnalysis(tuple):
+    result = ScNLP.annotate(tuple[0], properties = {'annotators':'sentiment', 'outputFormat': 'json'})
+    sentiment = result['sentences'][0]['sentiment']
+    location = tuple[1]
+    latitude, longitude = location.split(';;;;')
+    return sentiment.location
 
 TCP_IP = 'localhost'
 TCP_PORT = 9001
@@ -19,10 +29,14 @@ ssc.checkpoint("checkpoint_TwitterApp")
 dataStream = ssc.socketTextStream(TCP_IP, TCP_PORT)
 
 ######### your processing here ###################
-dataStream.pprint()
-words = dataStream.flatMap(lambda x: x.split(' '))
-wordcount = words.map(lambda x: (x,1)).reduceByKey(lambda x,y: x+y)
-wordcount.pprint()
+# dataStream.pprint()
+words = dataStream.flatMap(lambda x: x.split('||#||'))
+tokens = words.filter(lambda x: len(x.split('::::')) == 2)
+processed = tokens.map(lambda x: x.split('::::'))
+# wordcount = words.map(lambda x: (x,1)).reduceByKey(lambda x,y: x+y)
+# wordcount.pprint()
+sentiments = processed.map(lambda x: sentimentalAnalysis(x))
+sentiments.pprint()
 #################################################
 
 ssc.start()
